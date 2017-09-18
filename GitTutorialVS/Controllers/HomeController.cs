497e497275f62278a030d86383e2bec0a1cfbb70
Maria Silva <NB23238@novabase.pt>
2017-09-18 16:58:56 +0100

    ascas

--- GitTutorialVS/Controllers/HomeController.cs
+++ GitTutorialVS/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace GitTutorialVS.Controllers
     {
         public ActionResult Index()
         {
-        //edited on git hub
+            //edited on git hub
+            //edited on git hub
             return View();
         }
 
